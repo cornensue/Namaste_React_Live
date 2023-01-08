a76e0b905667219096bb3817d3dcaf68e54bc5d9
Cornelio Nsue <mastarichy8@gmail.com>
2023-01-08 21:14:46 +0100

    Starting assignments chapter 3

---
+++ Chapter_03-Laying-the-Foundation/assignments/coding/App.js
@@ -0,0 +1,22 @@
+import ReactDOM, {createRoot} from 'react-dom/client';
+
+const heading1 = React.createElement(
+	"h1", 
+	{
+		id: 'title1',
+	},
+	"Assignments - Igniting our App"
+);
+
+const container = React.createElement(
+	"div", 
+	{
+		id: 'container',
+	},
+	[heading1]
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// passing a react element inside root
+// async defer
+root.render(container);
