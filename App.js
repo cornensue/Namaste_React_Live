c0b9d0834f55d9687379661775b5e77df19a12b6
Cornelio Nsue <mastarichy8@gmail.com>
2023-01-09 01:08:16 +0100

    creating our first app

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
 import ReactDOM, {createRoot} from 'react-dom/client';
-
+/*
+	Header
+		- Logo
+		- Nav Items(Right side)
+		Car
+	Body
+		- Search bar
+		- Restaurant list
+			- RestaurantCard
+				- Image
+				- Name
+				- Rating
+				- Cusines
+	Footer
+		- Links
+*/
 const Title = () => (
-	<h1 id='title2' key="h2">
-		Namaste JSX
-	</h1>
+	<a href='/'>
+		<img className="logo" alt="our logo" src="http://lh3.googleusercontent.com/Em7AHf7XBH_RtGfCBVXz9RH8SM_pHkj3xPP-yd3cRguY1_Jc8fmqgx6WxnvGVyPV5xs5gL3HCD0FCuv6Xo4CwoY6ak4"
+		/>
+	</a>
 );
 
 // Composing Components
 const HeaderComponent = () => {
 	return (
-		<div>
-		{<Title />}
-			<h2>Namaste React Functional Component</h2>
-			<h3>This is a h2 tag</h3>
+		<div className="header">
+			<Title />
+			<div className="nav-items">
+				<ul>
+					<li>Home</li>
+					<li>About</li>
+					<li>Contact</li>
+					<li>Cart</li>
+				</ul>
+			</div>
 		</div>
 	)
 };
+
+const Header = () => {
+	return (
+		<h1>Header here</h1>
+	);
+};
+
+const Body = () => {
+	return (
+		<h1>Body</h1>
+	);
+};
+
+const Footer = () => {
+	return (
+		<h1>Footer</h1>
+	);
+};
+
+const AppLayout = () => {
+	return (
+		<>
+			<Header/>
+			<Body/>
+			<Footer/>
+		</>
+	);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
