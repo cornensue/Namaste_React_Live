add66e94687dd7cd8741f6fe27c0847d9684700f
Cornelio Nsue <mastarichy8@gmail.com>
2024-04-23 00:10:13 +0100

    finished chapter 8

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,19 @@
-const Profile = () => {
+import { useState } from "react";
+
+const Profile = (props) => {
+   const [count, setCount] = useState(0);
    return (
       <div>
          <h1>Profile Component</h1>
+         <h2>Name: {props.name}</h2>
+         <h3>Count: {props.count}</h3>
+         <button
+            onClick={() => {
+               setCount(1);
+            }}
+         >
+            Count
+         </button>
       </div>
    );
 };
