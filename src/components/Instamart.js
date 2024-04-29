4594dbdbfeed0548784f67a3cdf6c87e6fe79791
Cornelio Nsue <mastarichy8@gmail.com>
2024-04-30 00:55:44 +0100

    chapter 9 finished

---
+++ src/components/Instamart.js
@@ -0,0 +1,12 @@
+const Instamart = () => {
+   return (
+      <>
+         <section>
+            <h1>Instamart</h1>
+            <h2>Supose, there is 100 more components inside here</h2>
+         </section>
+      </>
+   );
+};
+
+export default Instamart;
